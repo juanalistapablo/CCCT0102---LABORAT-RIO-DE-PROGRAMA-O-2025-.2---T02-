ab5cb9048b674a6f14c92c5fe96bfd7d69013674
JUANPABLOMONDEGO <juanmondego208@gmail.com>
2025-10-11 19:38:11 +0000

    Atualiza entrada de dados para receber input do usuário

---
+++ Modolo1/entrada e saída de dados.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    
+    printf("Digite um valor inteiro para x: "); // Pede ao usuário para inserir um valor
+    scanf("%d", &x); // Lê o valor inteiro digitado e armazena em x
+    
+    printf("O valor de x e: %d\n",x); // Imprime o valor de x que foi lido
+    /* code */
+    return 0;
+}
---
+++ Modolo1/introdução a linguagemC.c
@@ -0,0 +1,11 @@
+#include<stdio.h> // Biblioteca padrão de entrada e saída
+int main(){ // Função principal do programa
+    int n1,n2,n3,m;
+    n1=10; // Atribui o valor 10 à variável n1
+    n2=20; // Atribui o valor 20 à variável n2
+    n3=30; // Atribui o valor 30 à variável n3
+    m=(n1+n2+n3)/3; // Calcula a média dos três números
+    printf("A media dos tres numeros e: %d\n",m); // Imprime
+     // Imprime a mensagem "Hello World!" na tela
+    return 0; // Indica que o programa terminou com sucesso
+}
