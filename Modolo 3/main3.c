d639da061b247a28b9cc458db7151ec553d4225a
JUANPABLOMONDEGO <juanmondego208@gmail.com>
2026-01-05 18:48:42 -0300

    Add complex number and family structure programs

---
+++ Modolo 3/main3.c
@@ -0,0 +1,66 @@
+
+#include <stdio.h>
+#include <math.h>
+
+struct Complexo {
+    float real;
+    float imaginaria;
+};
+
+int main() {
+    struct Complexo z;
+    float modulo;
+
+    printf("Digite a parte real (a): ");
+    scanf("%f", &z.real);
+
+    printf("Digite a parte imaginaria (b): ");
+    scanf("%f", &z.imaginaria);
+
+    modulo = sqrt((z.real * z.real) + (z.imaginaria * z.imaginaria));
+
+    printf("O modulo de z eh: %.2f\n", modulo);
+
+    return 0;
+}
+*/
+
+
+#include <stdio.h>
+
+struct Familia {
+    char sobrenome_pai[50];
+    char sobrenome_mae[50];
+    int qtd_filhos;
+    int qtd_filhas;
+    int qtd_animais;
+};
+
+int main() {
+    struct Familia f;
+
+    printf("Digite o sobrenome do pai: ");
+    scanf("%s", f.sobrenome_pai);
+
+    printf("Digite o sobrenome da mae: ");
+    scanf("%s", f.sobrenome_mae);
+
+    printf("Quantidade de filhos homens: ");
+    scanf("%d", &f.qtd_filhos);
+
+    printf("Quantidade de filhas mulheres: ");
+    scanf("%d", &f.qtd_filhas);
+
+    printf("Quantidade de animais: ");
+    scanf("%d", &f.qtd_animais);
+
+    printf("\n--- Dados Cadastrados ---\n");
+    printf("Sobrenome Pai: %s\n", f.sobrenome_pai);
+    printf("Sobrenome Mae: %s\n", f.sobrenome_mae);
+    printf("Filhos: %d\n", f.qtd_filhos);
+    printf("Filhas: %d\n", f.qtd_filhas);
+    printf("Animais: %d\n", f.qtd_animais);
+
+    return 0;
+}
+*/
